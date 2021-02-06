88fbfbe796210a6e0af4b9cdb24245d9b48681c6
Krysthyan <krysthyan.09h@gmail.com>
2021-02-06 11:07:15 -0500

    refactor code

--- lib/src/config/language.dart
+++ lib/src/config/language.dart
@@ -1,6 +1,4 @@
-
 class LoginFreshWords {
-
   String loginWith;
   String login;
   String exploreApp;
@@ -17,21 +15,19 @@ class LoginFreshWords {
 
   String messageRecoverPassword;
 
-
-  LoginFreshWords({
-        this.loginWith = 'Login With',
-    this.hintName = 'Name',
-    this.hintSurname = 'Surname',
-    this.hintSignUpRepeatPassword = 'Repeat Password',
-    this.hintLoginPassword = 'Password',
-    this.recoverPassword = 'Recover Password',
-    this.messageRecoverPassword = 'To recover the password, enter the email and press send email, you will receive an email so you can update your password. Only available for accounts created by username and password',
-    this.hintLoginUser = 'Username or email',
-    this.login = 'Login',
-        this.exploreApp = 'Explore App',
-        this.notAccount = 'You dot not have an account?',
-        this.signUp = 'Sign Up',
-    this.textLoading = 'please wait ...'
-  });
-
+  LoginFreshWords(
+      {this.loginWith = 'Login With',
+      this.hintName = 'Name',
+      this.hintSurname = 'Surname',
+      this.hintSignUpRepeatPassword = 'Repeat Password',
+      this.hintLoginPassword = 'Password',
+      this.recoverPassword = 'Recover Password',
+      this.messageRecoverPassword =
+          'To recover the password, enter the email and press send email, you will receive an email so you can update your password. Only available for accounts created by username and password',
+      this.hintLoginUser = 'Username or email',
+      this.login = 'Login',
+      this.exploreApp = 'Explore App',
+      this.notAccount = 'You dot not have an account?',
+      this.signUp = 'Sign Up',
+      this.textLoading = 'please wait ...'});
 }
